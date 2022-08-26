0e898deeb33dc3859d209982e80c8bc2da6823b3
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-26 01:45:19 +0000

    Create app.js

---
+++ Practice1/staticFileFun/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Request IP: " + req.url);
+  console.log("Request Date: " + new Date());
+  next();
+});
+
+app.use((req, res, next) => {
+  const filepath = path.join(__dirname, "static", req.url);
+  fs.exists(filepath, (exists) => {
+    if (exists) {
+      res.sendFile(filepath);
+    } else {
+      next();
+    }
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send("File not found");
+});
+
+app.listen(3000, () => {
+  console.log("App started on port 3000");
+});
